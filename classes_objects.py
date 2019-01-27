920e68be26f2afa4dfabdd2b5c61342a6c352fc8
rifkegribenes <rifkegribenes@gmail.com>
2019-01-27 14:20:22 -0800

    classmethods and staticmethods

--- classes_objects.py
+++ classes_objects.py
@@ -29,8 +29,19 @@ class Student:
     def average(self):
         return sum(self.marks) / len(self.marks)
 
+    @staticmethod
+    # @staticmethod and @classmethod are decorators
+    # static method passes no parameters
+    # class method passes the class as the first parameter
+    # normal methods pass self as the first parameter
+    def go_to_school():
+        print("I'm going to school.")
+
 anna = Student('Anna', 'MIT')
+rolf = Student('Rolf', 'Oxford')
+
 anna.marks.append(56)
 anna.marks.append(77)
 anna.marks.append(86)
 print(anna.average())
+Student.go_to_school()
