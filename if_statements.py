37229464225eb403e2caff4a0d727fd07049149c
rifkegribenes <rifkegribenes@gmail.com>
2019-01-27 13:15:37 -0800

    exercise

--- if_statements.py
+++ if_statements.py
@@ -1,11 +1,41 @@
-should_continue = True
-if should_continue:
-    print("Hello")
+# should_continue = True
+# if should_continue:
+#     print("Hello")
+#
+# known_people = ["John", "Anna", "Mary"]
+# person = input("Enter the person you know: ")
+#
+# if person in known_people:
+#     print("You know {}!".format(person))
+# else:
+#     print("You don't know {}!".format(person))
 
-known_people = ["John", "Anna", "Mary"]
-person = input("Enter the person you know: ")
 
-if person in known_people:
-    print("You know {}!".format(person))
-else:
-    print("You don't know {}!".format(person))
+## Exercise
+
+def who_do_you_know():
+    # Ask the user for a list of people they know
+    people = input("Enter the names of people you know, separated by commas: ")
+
+    # Split the string into a list
+    people_list = people.split(',')
+
+    people_without_spaces = []
+    for person in people_list:
+        people_without_spaces.append(person.strip())
+
+    # Return that list
+    return people_without_spaces
+
+def ask_user():
+    # Ask user for a name
+    person = input("Enter a name of someone you know: ")
+
+    # See if their name is in the list of people they know
+    if person in who_do_you_know():
+        print("You know {}".format(person))
+    else:
+        print("You don't know {}".format(person))
+
+ask_user()
+
