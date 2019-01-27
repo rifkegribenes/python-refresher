8f0a10a0d55bba28e31afc0021542c273d106b01
rifkegribenes <rifkegribenes@gmail.com>
2019-01-27 14:14:50 -0800

    classes and objects

---
+++ classes_objects.py
@@ -0,0 +1,36 @@
+lottery_player_dict = {
+    'name': 'Rolf',
+    'numbers': (5, 9, 12, 3, 1, 21)
+}
+
+class LotteryPlayer:
+    def __init__(self, name):
+        self.name = name,
+        self.numbers = (5, 9, 12, 3, 1, 21)
+
+    def total(self):
+        return sum(self.numbers)
+
+player_one = LotteryPlayer("Rolf")
+player_one.numbers = (1, 2, 3, 6, 7, 8)
+player_two = LotteryPlayer("John")
+
+# print(player.name)
+# print(player.total())
+
+##
+
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+anna = Student('Anna', 'MIT')
+anna.marks.append(56)
+anna.marks.append(77)
+anna.marks.append(86)
+print(anna.average())
---
+++ dictionary.py
@@ -0,0 +1,23 @@
+my_set = {1, 2, 3}
+my_dict = { 'name': "Jose", 'age': 90, 'grades': [13, 45, 66]}
+another_dict = { 1: 15, 2: 75, 3: 150 }
+
+lottery_player = {
+    'name': 'Rolf',
+    'numbers': (13, 45, 66, 23, 22)
+}
+
+universities = [
+    {
+        'name': 'Oxford',
+        'location': 'UK'
+    },
+    {
+        'name': 'MIT',
+        'location': 'US'
+    }
+]
+
+##
+
+sum(lottery_player['numbers'])
